5ca0ce79c8519f32965441c740c3b818284dd7fb
Vincent Robert <vincent.robert@nanego.com>
2020-08-03 16:43:51 +0200

    Add check for table in migration

--- db/migrate/002_create_project_typologies_table.rb
+++ db/migrate/002_create_project_typologies_table.rb
@@ -1,12 +1,14 @@
 class CreateProjectTypologiesTable < ActiveRecord::Migration[5.2]
   def self.up
-    create_table :project_typologies do |t|
-      t.column :typology_id, :integer, :null => false
-      t.column :project_id, :integer, :null => false
-      t.column :tracker_id, :integer
-      t.column :active, :boolean
+    unless ActiveRecord::Base.connection.table_exists? 'project_typologies'
+      create_table :project_typologies do |t|
+        t.column :typology_id, :integer, :null => false
+        t.column :project_id, :integer, :null => false
+        t.column :tracker_id, :integer
+        t.column :active, :boolean
+      end
+      add_index :project_typologies, [:project_id, :typology_id], :unique => true, :name => :projects_typologies_ids
     end
-    add_index :project_typologies, [:project_id, :typology_id], :unique => true, :name => :projects_typologies_ids
   end
 
   def self.down
