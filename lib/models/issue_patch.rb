3715b154b0747695141a308b3b1d97b449961d23
Vincent Robert <vincent.robert@nanego.com>
2020-10-08 12:14:58 +0200

    Typology field cannot be required if the module is disabled

--- lib/models/issue_patch.rb
+++ lib/models/issue_patch.rb
@@ -6,4 +6,33 @@ class Issue
 
   safe_attributes :typology_id
 
+  # Validates the issue against additional workflow requirements
+  def validate_required_fields
+    user = new_record? ? author : current_journal.try(:user)
+
+    required_attribute_names(user).each do |attribute|
+      if /^\d+$/.match?(attribute)
+        attribute = attribute.to_i
+        v = custom_field_values.detect {|v| v.custom_field_id == attribute }
+        if v && Array(v.value).detect(&:present?).nil?
+          errors.add :base, v.custom_field.name + ' ' + l('activerecord.errors.messages.blank')
+        end
+      else
+        if respond_to?(attribute) && send(attribute).blank? && !disabled_core_fields.include?(attribute)
+          next if attribute == 'category_id' && project.try(:issue_categories).blank?
+          next if attribute == 'fixed_version_id' && assignable_versions.blank?
+
+          #####
+          # START PATCH
+          next if attribute == 'typology_id' && !project.module_enabled?('typologies')
+          next if attribute == 'typology_id' && project.typologies.blank?
+          # END PATCH
+          #####
+
+          errors.add attribute, :blank
+        end
+      end
+    end
+  end
+
 end
--- spec/controllers/issues_controller_spec.rb
+++ spec/controllers/issues_controller_spec.rb
@@ -5,30 +5,30 @@ RSpec.describe IssuesController, type: :controller do
   render_views
 
   fixtures :users, :enumerations, :projects, :trackers, :issues,
-            :email_addresses, :user_preferences,
-            :roles,
-            :members,
-            :member_roles,
-            :issue_statuses,
-            :issue_relations,
-            :versions,
-            :projects_trackers,
-            :issue_categories,
-            :enabled_modules,
-            :enumerations,
-            :attachments,
-            :workflows,
-            :custom_fields,
-            :custom_values,
-            :custom_fields_projects,
-            :custom_fields_trackers,
-            :time_entries,
-            :journals,
-            :journal_details,
-            :queries,
-            :repositories,
-            :changesets,
-            :watchers
+           :email_addresses, :user_preferences,
+           :roles,
+           :members,
+           :member_roles,
+           :issue_statuses,
+           :issue_relations,
+           :versions,
+           :projects_trackers,
+           :issue_categories,
+           :enabled_modules,
+           :enumerations,
+           :attachments,
+           :workflows,
+           :custom_fields,
+           :custom_values,
+           :custom_fields_projects,
+           :custom_fields_trackers,
+           :time_entries,
+           :journals,
+           :journal_details,
+           :queries,
+           :repositories,
+           :changesets,
+           :watchers
 
   let!(:user) { User.find(1) }
   let!(:project) { Project.find(1) }
@@ -52,6 +52,77 @@ RSpec.describe IssuesController, type: :controller do
       get :new, params: {project_id: project.id}
       expect(response).to be_successful
       expect(response.body).to have_selector("label:contains('Typology')")
+    end
+  end
+
+  describe "new issue creation" do
+    it "creates a new issue with a typology" do
+      project.enable_module! :typologies
+
+      expect {
+        post :create, :params => {
+            :project_id => 1,
+            :issue => {
+                :tracker_id => 3,
+                :status_id => 2,
+                :subject => 'This is the test_new issue',
+                :description => 'This is the description',
+                :priority_id => 5,
+                :typology_id => typology_2.id
+            },
+        }
+      }.to change(Issue, :count).by(1)
+
+      issue = Issue.last
+      expect(issue).not_to be_nil
+      expect(issue.typology).to eq typology_2
+    end
+
+    it "does not create a new issue if typology is required and empty" do
+      project.enable_module! :typologies
+      WorkflowPermission.delete_all
+      WorkflowPermission.create!(:old_status_id => 1, :tracker_id => 2, :role_id => 1, :field_name => 'typology_id', :rule => 'required')
+      @request.session[:user_id] = 2
+
+      expect {
+        post :create, :params => {
+            :project_id => 1,
+            :issue => {
+                :tracker_id => 2,
+                :status_id => 1,
+                :subject => 'This is the test_new issue',
+                :description => 'This is the description'
+            },
+        }
+      }.to_not change(Issue, :count)
+
+      expect(response).to be_successful
+      expect(response.body).to match /Typology cannot be blank/
+    end
+
+    it "creates a new issue without a typology when module is NOT activated, even with 'required' workflow" do
+      project.disable_module! :typologies
+      WorkflowPermission.delete_all
+      WorkflowPermission.create!(:old_status_id => 1, :tracker_id => 2, :role_id => 1, :field_name => 'typology_id', :rule => 'required')
+      @request.session[:user_id] = 2
+
+      expect {
+        post :create, :params => {
+            :project_id => 1,
+            :issue => {
+                :tracker_id => 2,
+                :status_id => 1,
+                :subject => 'This is the test_new issue',
+                :description => 'This is the description'
+            },
+        }
+      }.to change(Issue, :count).by(1)
+
+      issue = Issue.last
+      expect(response).to redirect_to issue_path(issue)
+      expect(response.body).to_not match /Typology cannot be blank/
+      expect(issue).not_to be_nil
+      expect(issue.typology).to eq nil
     end
   end
 
