67deae8e4d7ef8e81ffb47905d9fff9bff2a04de
Vincent Robert <vincent.robert@nanego.com>
2020-10-30 11:23:57 +0100

    Do not show typology value in issues if module is not activated

--- app/overrides/issues/show.rb
+++ app/overrides/issues/show.rb
@@ -4,6 +4,8 @@ Deface::Override.new :virtual_path  => 'issues/show',
                      :insert_top    => 'div.attributes',
                      :text          => <<TYPO_FIELD
 <%= issue_fields_rows do |rows|
-  rows.left l(:field_typology), @issue.typology.try(:name), :class => 'typology'
+  if @issue.project.present? && @issue.project.module_enabled?(:typologies)
+    rows.left l(:field_typology), @issue.typology.try(:name), :class => 'typology'
+  end
 end %>
 TYPO_FIELD
