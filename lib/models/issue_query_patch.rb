fa9c3891bbe05038dcbafd8b6ff8747fbbbb4155
Vincent Robert <vincent.robert@nanego.com>
2020-11-12 14:52:48 +0100

    Make typology column sortable and groupable

--- lib/models/issue_query_patch.rb
+++ lib/models/issue_query_patch.rb
@@ -1,7 +1,11 @@
 require_dependency 'issue_query'
 
 class IssueQuery
-  self.available_columns << QueryColumn.new(:typology, :sortable => false, :groupable => false) if self.available_columns.select {|c| c.name == :typology}.empty?
+
+  self.available_columns.reject! {|c| c.name == :priority}
+  self.available_columns << QueryColumn.new(:priority, :sortable => "priorities.position", :default_order => 'desc', :groupable => true)
+  self.available_columns << QueryColumn.new(:typology, :sortable => "typologies.position", :groupable => true) if self.available_columns.select {|c| c.name == :typology}.empty?
+
 end
 
 module PluginTypologies
@@ -18,6 +22,19 @@ module PluginTypologies
 
     def typologies
       @typologies ||= (project.nil? ? Typology.active.sorted : project.typologies)
+    end
+
+    def joins_for_order_statement(order_options)
+      joins = [super]
+      if order_options
+        if order_options.include?('priorities')
+          joins << "LEFT OUTER JOIN #{IssuePriority.table_name} AS priorities ON priorities.id = #{queried_table_name}.priority_id"
+        end
+        if order_options.include?('typologies')
+          joins << "LEFT OUTER JOIN #{Typology.table_name} AS typologies ON typologies.id = #{queried_table_name}.typology_id"
+        end
+      end
+      joins.any? ? joins.join(' ') : nil
     end
 
   end
