741409e76ba497d0e6b2c8d0d4350d802cc9bd4d
Vincent Robert <vincent.robert@nanego.com>
2023-04-13 17:10:12 +0200

    Do not use tmp variable if not necessary

--- lib/models/tracker_patch.rb
+++ lib/models/tracker_patch.rb
@@ -3,10 +3,7 @@ require_dependency "tracker"
 class Tracker
 
   #Add typology_id to core fields 
-  temp = CORE_FIELDS.dup
-  temp << "typology_id"
-
-  CORE_FIELDS = temp
+  CORE_FIELDS = CORE_FIELDS.dup | ["typology_id"]
   CORE_FIELDS_ALL = (CORE_FIELDS_UNDISABLABLE + CORE_FIELDS).freeze
 
   has_and_belongs_to_many :project_typologies
