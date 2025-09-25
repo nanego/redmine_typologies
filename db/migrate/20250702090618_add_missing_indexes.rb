a27103154434d68038bc3415b4f2cd97206af4ab
Vincent <contact@vincent-robert.com>
2025-09-25 08:13:47 +0200

    Remove obsolete CI workflow files

--- db/migrate/20250702090618_add_missing_indexes.rb
+++ db/migrate/20250702090618_add_missing_indexes.rb
@@ -1,7 +1,7 @@
-class AddMissingIndexes < ActiveRecord::Migration[7.2]
+class AddMissingIndexes < ActiveRecord::Migration[6.1]
   def change
     add_index :project_typologies, :project_id, if_not_exists: true
     add_index :project_typologies, :typology_id, if_not_exists: true
-    add_index :project_typologies_trackers, [:project_typology_id, :tracker_id], if_not_exists: true
+    add_index :project_typologies_trackers, [:project_typology_id, :tracker_id], if_not_exists: true, name: "index_project_typologies_trackers"
   end
 end
