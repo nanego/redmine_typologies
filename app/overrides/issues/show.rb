a5229a94a1a27d0ba820779765123e5c49b5bb31
Vincent <contact@vincent-robert.com>
2026-07-22 08:45:56 +0200

    Migrate legacy icon classes to SVG sprite_icon for Redmine 7.0

--- app/overrides/issues/show.rb
+++ app/overrides/issues/show.rb
@@ -1,6 +1,5 @@
 Deface::Override.new :virtual_path  => 'issues/show',
                      :name          => 'add-typology-field',
-                     :original      => '8f374ec2439b27545906aae44228462dfe14d196',
                      :insert_top    => 'div.attributes',
                      :text          => <<TYPO_FIELD
 <%= issue_fields_rows do |rows|
